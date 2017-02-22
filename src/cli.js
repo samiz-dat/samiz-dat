cdeea3eeb8d088339fc253172da3f8e9901c0e88
sdockray <sean.patrick.dockray@gmail.com>
2017-02-23 10:40:35 +1100

    CLI documentation

--- src/cli.js
+++ src/cli.js
@@ -13,11 +13,13 @@ function init_catalog() {
 	return new Catalog(dataDir);
 }
 
+// npm run cli --numAuthors 
 if (process.env.npm_config_numAuthors) {
 	const c = init_catalog();
 	const a = c.getAuthors(function(err, docs) {
 		console.log(`There are ${docs.length} authors`);
 	});
+// npm run cli --listAuthors 
 } else if (process.env.npm_config_listAuthors) {
 	const c = init_catalog();
 	const a = c.getAuthors(function(err, docs) {
@@ -28,4 +30,5 @@ if (process.env.npm_config_numAuthors) {
 }
 else {
 	console.log("--numAuthors\tPrint the number of authors in the catalog");
+	console.log("--listAuthors\tLists the authors in the catalog");
 }
