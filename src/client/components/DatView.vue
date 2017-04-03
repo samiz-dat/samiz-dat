805d783d129ac034eaa556e65d909c5937125726
e-e-e <e-e-e@users.noreply.github.com>
2017-04-03 19:00:38 +1000

    Beginings of DatView

---
+++ src/client/components/DatView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Dat
+  </div>
+</template>
+
+<script>
+  // import { mapActions } from 'vuex';
+
+  export default {
+    name: 'DatView',
+    components: {},
+    data() {
+      return {};
+    },
+    methods: {},
+};
+</script>
+
+<style lang="css">
+  body, html {  }
+</style>
