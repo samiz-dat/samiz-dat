699b8cfa47edeeda757852024d65d87beef5e333
sdockray <sean.patrick.dockray@gmail.com>
2017-07-18 14:07:30 +1000

    mockup of home screen, adding remote dats from there

--- src/renderer/components/datImportField.vue
+++ src/renderer/components/datImportField.vue
@@ -1,10 +1,10 @@
 <template>
   <el-form ref="form">
     <el-form-item label="Library Key">
-      <el-input name="key" v-model="key" placeholder="Dat Key"></el-input><br />
+      <el-input name="key" v-model="libraryInfo.key" placeholder="Dat Key"></el-input><br />
     </el-form-item>
     <el-form-item label="Name">
-      <el-input name="name" v-model="name" placeholder="Name it"></el-input><br />
+      <el-input name="name" v-model="libraryInfo.name" placeholder="Name it"></el-input><br />
     </el-form-item>
     <el-form-item>
       <el-button type="primary" v-on:click="submit($event)">Import</el-button>
@@ -18,11 +18,14 @@
   export default {
     name: 'DatImportField',
     components: {},
+    props: {
+      libraryInfo: {
+        type: Object,
+        default: { key: '', name: '' },
+      },
+    },
     data() {
-      return {
-        key: '',
-        name: '',
-      };
+      return {};
     },
     methods: {
       ...mapActions(['importDat']),
@@ -30,6 +33,10 @@
         if (event) event.preventDefault();
         const { key, name } = this;
         this.importDat({ key, name });
+        this.$notify({
+          title: 'Import',
+          message: 'Starting to import library!',
+        });
       },
     },
 };
