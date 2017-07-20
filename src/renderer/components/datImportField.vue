31733f891784fbf371ab5768bae463f32dc136f1
sdockray <sean.patrick.dockray@gmail.com>
2017-07-20 22:31:43 +1000

    adding some text

--- src/renderer/components/datImportField.vue
+++ src/renderer/components/datImportField.vue
@@ -1,5 +1,11 @@
 <template>
   <el-form ref="form">
+    <p>
+    Adding a remote library will, at first, only download that library's basic catalogue for searching. You can then choose to download the entire library or only selected titles or authors within it.
+    </p>
+    <p>
+    By downloading a library you also help contribute to the hosting and distribution of that library.
+    </p>
     <el-form-item label="Library Key">
       <el-input name="key" v-model="key" placeholder="Dat Key"></el-input><br />
     </el-form-item>
--- src/renderer/containers/home.vue
+++ src/renderer/containers/home.vue
@@ -3,53 +3,77 @@
     <h2>About Dat Library</h2>
     <div class="content">
       <div class="left">
-        <h3>In a few words:</h3>
-        <p>
-        Intentions victorious truth evil aversion morality christian love. Salvation morality prejudice faithful christianity zarathustra ultimate decrepit ascetic philosophy intentions convictions intentions. Eternal-return sexuality right salvation burying will reason ubermensch salvation. Moral endless ideal zarathustra joy. Morality snare christianity intentions enlightenment ultimate aversion. Burying right convictions battle free philosophy contradict contradict virtues contradict pious ubermensch victorious virtues. You might add <el-button type="text" @click="openImportDialog('1943c202c68e66231ec5c2bde9e17ff6103011176515931fcb8eed3415ba8849', 'Random')">Another hosted by Dat Library</el-button>
-        </p><p>
-        Or there is this one: <el-button type="text" @click="openImportDialog('ea17a8f2a05b6dc7f8556ec76b77b966004dec711c313bb8564a4895e2955cc0', 'Grr')">The Grr Testing Library</el-button>.
-        Madness dead spirit aversion grandeur justice horror pious. Burying passion madness law pinnacle overcome spirit marvelous oneself deceptions ocean horror transvaluation society.
-        </p><p>
-        Of sea endless noble revaluation pious. Philosophy abstract virtues christianity evil. Ocean fearful selfish abstract gains spirit joy contradict.
-        </p><p>
-        Moral pinnacle derive oneself pinnacle. Ocean reason disgust victorious war fearful. Ascetic ultimate derive against against morality spirit deceptions burying noble prejudice. Chaos philosophy intentions christian inexpedient burying joy ocean love.
-        </p><p>
-        Ascetic ultimate ascetic burying self love abstract of. Christianity pinnacle right ideal horror. Holiest depths battle chaos hope of overcome holiest virtues morality grandeur madness against. Pinnacle virtues war spirit chaos virtues grandeur pious ubermensch revaluation of horror contradict value. Oneself christianity merciful derive ocean pious prejudice derive marvelous derive madness. Insofar truth joy of ideal.
-        </p><p>
-        Insofar decieve burying moral of horror transvaluation gains holiest. Depths against decrepit society marvelous. Inexpedient love suicide free pious superiority strong. Joy snare against ideal transvaluation deceptions enlightenment battle. Superiority burying chaos christian passion play dead will ubermensch suicide truth moral free. Convictions contradict philosophy morality superiority ocean superiority law derive ultimate philosophy oneself god.
-        </p><p>
-        Burying pinnacle ocean merciful philosophy against enlightenment convictions spirit. Depths value self abstract holiest.
-        </p><p>
-        Hope faithful war good moral justice holiest grandeur burying deceptions mountains love overcome sea. Madness ultimate transvaluation right ideal ultimate hatred love ultimate. Salvation transvaluation endless self eternal-return decrepit burying endless. Holiest holiest revaluation deceptions joy strong fearful christianity. Convictions convictions faith passion convictions value.
-        </p>
+          <p>
+          <span class='quote'>“THE LIBRARY IS A GROWING ORGANISM.”</span><br/>
+          <span class='quote'>S.R Ranganathan</span>
+          </p>
+
+          <p><b>TLDR;</b></p>
+
+          <p>
+          <span>Dat Library is</span>
+          <ul>
+          <li>a library of libraries</li>
+          <li>an approach to archiving</li>
+          <li>peer to peer</li>
+          <li>between us</li>
+          <li>decentralised and distributed</li>
+          <li>built by its users/readers</li>
+          <li>open source</li>
+          <li>free</li>
+          <li>an invitation</li>
+          <li>an out stretched hand</li>
+          <li>cooperation</li>
+          <li>a work in progress</li>
+          <li>open to suggestion</li>
+          <li>for artists, poets, academics, and like-minded friends</li>
+          <li>a tool for research</li>
+          <li>a way of sharing</li>
+          <li>built on top of the Dat Protocol</li>
+          <li>this text will be updated with software updates</li>
+          </ul>
+          </p>
+
+          <p><b>OR:</b></p>
+
+          <p>
+          Let’s say you have a collection of an out-of-print, obscure, radical zine from Sydney in the 1970s; or you have collected the writings of your comrades; or you have a directory of your own texts that are currently unpublished or otherwise inaccessible. With Dat Library you can share them easily with others.
+          </p>
+          <p>
+          Dat Library is a library of libraries. Every one of these libraries has their own public key. Using a libraries key, you can access that library. Sharing the key for your own libraries lets others access yours. What makes Dat Libraries special is that there is no central server hosting the files. The files are collectively hosted and distributed by the readers of those libraries. By downloading libraries and keeping the application running, you help provide mutual support in the form of disk space and bandwidth and redundancy for the libraries you have downloaded.
+          </p>
+          <p>
+          <b>How do I use it?</b>
+          </p>
+          <p>
+          Get started by downloading the starter libraries. Make your own library, or import your personal calibre library. Then share your library’s key with others to start distributing and collectively archiving your collection.
+          </p>
+          <p>
+          Collections are private - you need to share your key with others to make your library accessible. If you would like to share your library on this page, and you hold the permissions to share, please get in touch and we will add your dats key to this page in future releases of the app.
+          </p>
+
       </div>
       <div class="right">
         <h3>Get started</h3>
-        <p>some text baout the dat keys</p>
+        <p>You can import these libraries made by our friends at <a href="https://www.memoryoftheworld.org/">Memory of the World</a>:</p>
         <ul>
           <li>
             <el-button
               type="text"
-              @click="openImportDialog('ea17a8f2a05b6dc7f8556ec76b77b966004dec711c313bb8564a4895e2955cc0', 'Grr')"
+              @click="openImportDialog('9c7000896467f2c9a3137affb01363a12caa23b0cdea6d2abe3b7b8de07f752d', 'Praxis')"
             >
-              The Grr Testing Library
-            </el-button>
+              Praxis (digitized).
+            </el-button><br/>
+            A collection containing the (digitized) issues of Yugoslav, international, special and pocket editions of “Praxis”.
           </li>
           <li>
             <el-button
               type="text"
-              @click="openImportDialog('ea17a8f2a05b6dc7f8556ec76b77b966004dec711c313bb8564a4895e2955cc0', 'Grr')"
+              @click="openImportDialog('94c7e648c66c1ccab224868d42f66bd9fcc949ee4714935d739555eca2385e45', 'Midnight Notes')"
             >
-              The Grr Testing Library
-            </el-button>
-          </li>
-          <li>
-            <el-button
-              type="text"
-              @click="openImportDialog('ea17a8f2a05b6dc7f8556ec76b77b966004dec711c313bb8564a4895e2955cc0', 'Grr')"
-            >
-              The Grr Testing Library
-            </el-button>
+              Midnight Notes (digitized)
+            </el-button><br/>
+             A collection compiling the (digitized) issues of issues of Midnight Notes.
           </li>
         </ul>
       </div>
@@ -98,6 +122,10 @@
     }
     .right {
       margin-left: 1rem;
+    }
+    .quote {
+      font-size: 1.2em;
+      color:#CCC;
     }
   }
 </style>
