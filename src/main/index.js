645850d5004087a0268ddc297ff21e5a6e34f932
e-e-e <e-e-e@users.noreply.github.com>
2017-07-22 16:54:28 +1000

    automatically look for new releases from git repo.

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,11 @@
-import { app, Menu, BrowserWindow } from 'electron' // eslint-disable-line
-import { autoUpdater } from 'electron-updater';
-
+import { app, dialog, shell, Menu, BrowserWindow } from 'electron'; // eslint-disable-line
+import updateNotification from './updateNotification';
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\'); // eslint-disable-line
 }
 
 let mainWindow;
@@ -36,7 +35,8 @@ function createWindow() {
       label: 'Application',
       submenu: [
         { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit(); } },
-      ] },
+      ],
+    },
     {
       label: 'Edit',
       submenu: [
@@ -57,7 +57,10 @@ function createWindow() {
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  updateNotification();
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -66,23 +69,8 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
+  updateNotification();
   if (mainWindow === null) {
     createWindow();
   }
-});
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall();
-});
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates();
 });
---
+++ src/main/updateNotification.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import semver from 'semver';
+import { dialog, shell } from 'electron'; // eslint-disable-line
+
+// TODO: refactor this to be reuseable outside of this context
+const github = axios.create({ baseURL: 'https://api.github.com' });
+let ignore = false;
+
+export default function () {
+  if (ignore) return;
+  github.get('/repos/e-e-e/dat-library/releases/latest')
+    .then((res) => {
+      if (semver.lt(process.env.npm_package_version, res.data.tag_name)) {
+        dialog.showMessageBox({
+          type: 'info',
+          buttons: ['Not now', 'Update'],
+          defaultId: 1,
+          cancelId: 0,
+          title: 'Update Nofification',
+          message: 'There is a new version of Dat Library Available.',
+          detail: `You are running v${process.env.npm_package_version}.\nThe latest version is ${res.data.tag_name}.\n\nYou will need to download and install the latest version manually to update. Click update to view the latest version.`,
+          checkboxLabel: 'Don’t tell me again.',
+        }, (clickedId, checked) => {
+          if (clickedId === 1) {
+            shell.openExternal(res.data.html_url);
+          }
+          if (checked) {
+            ignore = true;
+          }
+        });
+      }
+    })
+    .catch((e) => {
+      // silently fail
+      console.log(e);
+    });
+}
